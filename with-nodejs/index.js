e6a1d316adf60b01e59fc17a9a931ae38f36c3c4
Raouf Chebri <raoufchebri@Raoufs-MacBook-Pro.local>
2022-10-21 11:29:24 +0200

    add with-nodejs example

---
+++ with-nodejs/index.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const URL = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}?sslmode=require&options=project%3D${process.env.PROJECT_NAME}`;
+
+const connect = async () => {
+  try {
+    const client = new Client(URL);
+    await client.connect();
+    const res = await client.query("SELECT 'Hello Neon' as message");
+    console.log(res.rows[0].message);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
