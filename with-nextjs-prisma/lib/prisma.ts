5cd65b7002b295d8d33d437ee2699037439f345e
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-02-27 01:36:37 +0200

    add connect_timeout and pool_timeout query params

--- with-nextjs-prisma/lib/prisma.ts
+++ with-nextjs-prisma/lib/prisma.ts
@@ -9,7 +9,7 @@ export const prisma =
       db: {
         // when using a pooled database connection with prisma, you need to append`?pgbouncer=true` to the connection string.
         // The reason this is done here rather than in the .env file is because the Neon Vercel integration doesn't include it.
-        url: `${process.env.DATABASE_URL}?pgbouncer=true`,
+        url: `${process.env.DATABASE_URL}?pgbouncer=true&connect_timeout=10&pool_timeout=10`,
       },
     },
   });
