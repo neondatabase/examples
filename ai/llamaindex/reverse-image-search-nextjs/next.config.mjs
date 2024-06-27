8f8cd1fdd26c1eef95847e6d10a437098a4af2eb
Rishi Raj Jain <jain71000@gmail.com>
2024-06-28 03:30:36 +0530

    remove experimental

--- ai/llamaindex/reverse-image-search-nextjs/next.config.mjs
+++ ai/llamaindex/reverse-image-search-nextjs/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
