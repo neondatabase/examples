574dfda247a4d848036fd609cc649fb72bffc327
Rishi Raj Jain <rishi@neon.tech>
2024-08-01 13:52:14 +0530

    use native by default

--- with-knex/index.tsx
+++ with-knex/index.tsx
@@ -1,15 +1,44 @@
-import "dotenv/config";
-import knex from "knex";
+import 'dotenv/config'
+import knex from 'knex'
+
+const connectionString = process.env.DATABASE_URL
 
 const client = knex({
-  client: "pg",
+  client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
   },
-});
+})
 
-const query = await client.raw("SELECT * from playing_with_neon");
+function replaceQueryParams(query, values) {
+  let replacedQuery = query
+  values.forEach((tmpParameter) => {
+    if (typeof tmpParameter === 'string') {
+      replacedQuery = replacedQuery.replace('?', `'${tmpParameter}'`)
+    } else {
+      replacedQuery = replacedQuery.replace('?', tmpParameter)
+    }
+  })
+  return replacedQuery
+}
 
-console.log(query.rows);
+async function executeQuery(text, values, normalize = false) {
+  if (normalize) return await client.raw(replaceQueryParams(text, values))
+  return await client.raw(text, values)
+}
 
-process.exit(1);
+await client.raw('SELECT 1')
+
+let s = performance.now()
+
+const query_1 = await executeQuery('SELECT * from playing_with_neon LIMIT ?', [5])
+
+console.log(performance.now() - s)
+
+s = performance.now()
+
+const query_2 = await executeQuery('SELECT * from playing_with_neon LIMIT ?', [5], true)
+
+console.log(performance.now() - s)
+
+process.exit(1)
