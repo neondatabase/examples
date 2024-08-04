97cfc45f5fb1734f1a940416c920915841b9ca89
Rishi Raj Jain <rishi@neon.tech>
2024-08-05 00:45:59 +0530

    with Flask

---
+++ with-flask/app.py
@@ -0,0 +1,28 @@
+import os
+import psycopg2
+from flask import Flask
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    p = urlparse(os.getenv("DATABASE_URL"))
+    return psycopg2.connect(
+        host=p.hostname,
+        database=p.path.replace("/", ""),
+        user=p.username,
+        password=p.password,
+        port=5432,
+    )
+
+
+@app.route("/")
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM playing_with_neon")
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return books
