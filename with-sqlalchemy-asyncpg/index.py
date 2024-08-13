9520db9be0969dbce5151b44b369111e5fdcf356
Rishi Raj Jain <rishi@neon.tech>
2024-08-13 20:06:46 +0530

    Update index.py

--- with-sqlalchemy-asyncpg/index.py
+++ with-sqlalchemy-asyncpg/index.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
 
 async def async_main() -> None:
-    engine = create_async_engine("postgresql+asyncpg://neondb_owner:***@ep-***.us-east-2.aws.neon.tech/neondb", echo=True)
+    engine = create_async_engine("postgresql+asyncpg://neondb_owner:***@ep-***.us-east-2.aws.neon.tech/neondb?ssl=require", echo=True)
     async with engine.connect() as conn:
         result = await conn.execute(text("select 'hello world'"))
         print(result.fetchall())
