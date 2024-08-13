e681193c9a485dbd49a007dc18aac1761b6f68b2
Rishi Raj Jain <rishi@neon.tech>
2024-08-13 20:10:57 +0530

    Update index.py

--- with-sqlalchemy-asyncpg/index.py
+++ with-sqlalchemy-asyncpg/index.py
@@ -1,9 +1,16 @@
+import os
 import asyncio
 from sqlalchemy import text
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 from sqlalchemy.ext.asyncio import create_async_engine
 
+load_dotenv()
+
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 async def async_main() -> None:
-    engine = create_async_engine("postgresql+asyncpg://neondb_owner:***@ep-***.us-east-2.aws.neon.tech/neondb?ssl=require", echo=True)
+    engine = create_async_engine(f"postgresql+asyncpg://{tmpPostgres.username}:{tmpPostgres.password}@{tmpPostgres.hostname}{tmpPostgres.path}?ssl=require", echo=True)
     async with engine.connect() as conn:
         result = await conn.execute(text("select 'hello world'"))
         print(result.fetchall())
