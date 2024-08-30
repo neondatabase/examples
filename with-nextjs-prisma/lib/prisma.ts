9463b06ef95be0e51b95f397c6716daaae93236c
Rishi Raj Jain <rishi@neon.tech>
2024-08-30 13:00:38 +0530

    Update prisma.ts

--- with-nextjs-prisma/lib/prisma.ts
+++ with-nextjs-prisma/lib/prisma.ts
@@ -7,9 +7,8 @@ export const prisma =
   new PrismaClient({
     datasources: {
       db: {
-        // when using a pooled database connection with prisma, you need to append`?pgbouncer=true` to the connection string.
         // The reason this is done here rather than in the .env file is because the Neon Vercel integration doesn't include it.
-        url: `${process.env.DATABASE_URL}?pgbouncer=true&connect_timeout=10&pool_timeout=10`,
+        url: `${process.env.DATABASE_URL}?connect_timeout=10&pool_timeout=10`,
       },
     },
   });
