5ebaa58d84fad16b294058834f507e529f9a125e
Rishi Raj Jain <rishi@neon.tech>
2024-09-07 08:29:43 +0530

    display date on the edge

--- with-nextjs-prisma-edge/components/table.tsx
+++ with-nextjs-prisma-edge/components/table.tsx
@@ -40,13 +40,14 @@ export default function Table() {
       <div className="divide-y divide-gray-900/5">
         {users.map((user) => (
           <div key={user.name} className="flex items-center justify-between py-3">
-            <div className="flex items-center space-x-4">
+            <div className="flex flex-row items-center space-x-4">
               <Image src={user.image} alt={user.name} width={48} height={48} className="rounded-full ring-1 ring-gray-900/5" />
               <div className="space-y-1">
                 <p className="font-medium leading-none">{user.name}</p>
                 <p className="text-sm text-gray-500">{user.email}</p>
               </div>
             </div>
+            <span className="text-gray-600">{new Date(user.createdAt).toDateString()}</span>
           </div>
         ))}
       </div>
