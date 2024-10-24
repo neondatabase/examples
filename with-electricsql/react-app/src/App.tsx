3885dbb539477131bd08d21be618bc2cebb8a028
Rishi Raj Jain <rishi@neon.tech>
2024-10-24 16:16:24 +0530

    formatting

--- with-electricsql/react-app/src/App.tsx
+++ with-electricsql/react-app/src/App.tsx
@@ -4,7 +4,11 @@ function Component() {
   const { data } = useShape({
     url: `http://localhost:3000/v1/shape/foo`,
   });
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>;
+    </>
+  );
 }
 
 export default Component;
