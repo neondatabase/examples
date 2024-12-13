1a6e47481d23d05e72f701e311aa3de0b46491c2
Rishi Raj Jain <rishi@neon.tech>
2024-12-13 17:30:39 +0530

    Update drizzle.server.ts

--- with-nextjs-drizzle-local-vercel/lib/drizzle.server.ts
+++ with-nextjs-drizzle-local-vercel/lib/drizzle.server.ts
@@ -2,7 +2,7 @@ import { neonConfig, Pool } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-serverless'
 import { WebSocket } from 'ws'
 
-const connectionString = process.env.VERCEL_ENV ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
+const connectionString = process.env.VERCEL_ENV === 'production' ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
 
 if (process.env.VERCEL_ENV) {
   neonConfig.webSocketConstructor = WebSocket
