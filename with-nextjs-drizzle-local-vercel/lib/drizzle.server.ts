8df6b1bb7572a5f4f37643d4456dfeaed1ea5b08
Rishi Raj Jain <rishi@neon.tech>
2024-12-17 02:21:00 +0530

    Update drizzle.server.ts

--- with-nextjs-drizzle-local-vercel/lib/drizzle.server.ts
+++ with-nextjs-drizzle-local-vercel/lib/drizzle.server.ts
@@ -1,16 +1,17 @@
-import { WebSocket } from 'ws'
-import { drizzle } from 'drizzle-orm/neon-serverless'
 import { neonConfig, Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
+import { WebSocket } from 'ws'
 
-const connectionString = process.env.VERCEL_ENV === 'production' ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
-if (process.env.VERCEL_ENV === 'production') {
-  neonConfig.poolQueryViaFetch = true
-  neonConfig.webSocketConstructor = WebSocket
+const connectionString = process.env.NODE_ENV === 'production' ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
+
+if (process.env.NODE_ENV === 'production') {
+   neonConfig.webSocketConstructor = WebSocket
+   neonConfig.poolQueryViaFetch = true
 } else {
-  neonConfig.pipelineTLS = false
-  neonConfig.pipelineConnect = false
-  neonConfig.useSecureWebSocket = false
-  neonConfig.wsProxy = (host) => `${host}:4444/v1`
+   neonConfig.wsProxy = (host) => `${host}:5433/v1`
+   neonConfig.useSecureWebSocket = false
+   neonConfig.pipelineTLS = false
+   neonConfig.pipelineConnect = false
 }
 
 const pool = new Pool({ connectionString })
