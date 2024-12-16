a4a071bd7afb010f9bcfb3f073f691fd598161f8
Rishi Raj Jain <rishi@neon.tech>
2024-12-17 02:21:10 +0530

    Update drizzle.config.ts

--- with-nextjs-drizzle-local-vercel/drizzle.config.ts
+++ with-nextjs-drizzle-local-vercel/drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'drizzle-kit'
 
-const url = process.env.VERCEL_ENV === 'production' ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
-if (!url) throw new Error(`Connection string to ${process.env.VERCEL_ENV ? 'Neon' : 'local'} Postgres not found.`)
+const url = process.env.NODE_ENV === 'production' ? process.env.POSTGRES_URL : process.env.LOCAL_POSTGRES_URL
+if (!url) throw new Error(`Connection string to ${process.env.NODE_ENV ? 'Neon' : 'local'} Postgres not found.`)
 
 export default defineConfig({
-  dialect: 'postgresql',
-  dbCredentials: { url },
-  schema: './lib/schema.ts',
+   dialect: 'postgresql',
+   dbCredentials: { url },
+   schema: './lib/schema.ts',
 })
