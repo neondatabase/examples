4394c1c7ff76fe344d97da5891f6fd6f4beef949
Philip Olson <philip@neon.tech>
2026-06-11 05:28:57 -0700

    chore: remove deprecated examples and refresh README (#219)

--- with-sveltekit/src/postgres.server.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config'
-import { neon } from '@neondatabase/serverless'
-
-const connectionString: string = process.env.DATABASE_URL as string
-
-export default neon(connectionString)
